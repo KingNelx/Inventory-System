843c1ab3eaa0b2b7aa89c0efce79e4b625a871b3
KingNelx <tapiajonel606@gmail.com>
2023-02-04 21:08:24 +0800

    setter and getter fix

--- Product.java
+++ Product.java
@@ -5,38 +5,76 @@ public class Product {
   private double productPrice;
   private String productBrand;
   private String productCategory;
-  private String emptySpace = " ";
+  private static final String emptySpace = " ";
 
   private static final String MSG_STRING_EMPTY = " Empty Text Detected ";
   private static final String MSG_CONTAIN_NULL = " Null Text Detected ";
+  private static final String MSG_INT_NEGATIVE = " Negative Number Detected ";
 
-  public void setProductID() {}
+  public void setProductID(int productID) {
+    if (productID < 0) {
+      throw new IllegalArgumentException(MSG_INT_NEGATIVE);
+    } else {
+      this.productID = productID;
+    }
+  }
 
-  public void setProductName() {}
+  public void setProductName(String productName) {
+    if (productName == null || productName.trim().isEmpty()) {
+      throw new IllegalArgumentException(MSG_CONTAIN_NULL);
+    }
+    if (productName.equals(emptySpace)) {
+      throw new IllegalArgumentException(MSG_STRING_EMPTY);
+    } else {
+      this.productName = productName;
+    }
+  }
 
-  public void setProductPrice() {}
+  public void setProductPrice(double productPrice) {
+    if (productPrice < 0) {
+      throw new IllegalArgumentException(MSG_INT_NEGATIVE);
+    } else {
+      this.productPrice = productPrice;
+    }
+  }
 
-  public void setProductBrand() {}
+  public void setProductBrand(String productBrand) {
+    if (productBrand == null || productBrand.trim().isEmpty()) {
+      throw new IllegalArgumentException(MSG_CONTAIN_NULL);
+    }
+    if (productBrand.equals(emptySpace)) {
+      throw new IllegalArgumentException(MSG_STRING_EMPTY);
+    } else {
+      this.productBrand = productBrand;
+    }
+  }
 
-  public void setProductCategory() {}
+  public void setProductCategory(String productCategory) {
+    if (productCategory == null || productCategory.trim().isEmpty()) {
+        throw new IllegalArgumentException(MSG_CONTAIN_NULL);
+    }
+    if (productCategory.equals(emptySpace)) {
+        throw new IllegalArgumentException(MSG_STRING_EMPTY);
+    }
+  }
 
-  public int getProductID () {
+  public int getProductID() {
     return productID;
   }
 
-  public String getProductName () {
+  public String getProductName() {
     return productName;
   }
 
-  public double getproductPrice () {
+  public double getProductPrice() {
     return productPrice;
   }
 
-  public String getProductBrand () {
+  public String getProductBrand() {
     return productBrand;
   }
 
-  public String getProductCategory () {
+  public String getProductCategory() {
     return productCategory;
   }
 }
