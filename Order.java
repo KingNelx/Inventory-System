064417491e45b94172e199f996f0a1e81507e4dd
KingNelx <tapiajonel606@gmail.com>
2023-02-06 11:31:44 +0800

    modified order class

--- Order.java
+++ Order.java
@@ -1,5 +1,35 @@
 public class Order {
+
+  private int orderID;
+  private String orderedProduct;
+  private int orderQuantity;
+
+  private static final String MSG_NEGATIVE_NUMBER_DETECTED =
+    " Negative Number Detected ";
+
+  public void setOrderID(int orderID) {
+
+    if (orderID < 0) {
+      throw new IllegalArgumentException(MSG_NEGATIVE_NUMBER_DETECTED);
+    } else {
+      this.orderID = orderID;
+    }
     
-    private int orderID;
-   
+  }
+
+  public void setOrderedProduct() {}
+
+  public void setOrderQuantity() {}
+
+  public int getOrderID() {
+    return orderID;
+  }
+
+  public String getOrderedProduct() {
+    return orderedProduct;
+  }
+
+  public int getOrderQuantiy() {
+    return orderQuantity;
+  }
 }
