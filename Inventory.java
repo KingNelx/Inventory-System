1b50052fe365aa4b118779e17816bf3859206838
KingNelx <tapiajonel606@gmail.com>
2023-02-07 23:48:57 +0800

    added array list for order class and Product class

--- Inventory.java
+++ Inventory.java
@@ -1,4 +1,23 @@
+import java.util.ArrayList;
 
 public class Inventory {
-    
+
+  ArrayList<Product> prodList = new ArrayList<>();
+  ArrayList<Order> ordList = new ArrayList<>();
+
+  public void addProducts(Product prod) {
+    prodList.add(prod);
+  }
+
+  public void removeProducts(Product prod) {
+    prodList.remove(prod);
+  }
+
+  public void addOrders(Order ord) {
+    ordList.add(ord);
+  }
+
+  public void removeOrders(Order ord) {
+    ordList.remove(ord);
+  }
 }
