f2b1a5e7f138ab521db3dbdba29b08d0697e39d5
KingNelx <tapiajonel606@gmail.com>
2023-02-12 10:51:22 +0800

    added more security features in Inventory class

--- Inventory.java
+++ Inventory.java
@@ -8,9 +8,14 @@ public class Inventory {
 
   private static final String MSG_PRODUCT_EMPTY = " Product List is Empty ";
   private static final String MSG_ORDER_EMPTY = " Order List is Empty ";
+  private static final String MSG_NEGATIVE_NUM = " Negative number detected ";
+  private static final String MSG_PRODUCT_NOT_FOUND =
+    " Product not found exception ";
+  private static final String MSG_ORDER_NOT_FOUND =
+    " Order not found exception ";
 
   public void addProduct(Product prod) {
-    if (product.isEmpty()) {
+    if (product.isEmpty() || product == null) {
       throw new IllegalArgumentException(MSG_PRODUCT_EMPTY);
     } else {
       product.add(prod);
@@ -18,11 +23,36 @@ public class Inventory {
   }
 
   public void addOrder(Order ord) {
-    if (order.isEmpty()) {
+    if (order.isEmpty() || product == null) {
       throw new IllegalArgumentException(MSG_ORDER_EMPTY);
     } else {
       order.add(ord);
     }
+  }
+
+  public Product getProductByID(int prodID) {
+    if (prodID < 0) {
+      throw new IllegalArgumentException(MSG_NEGATIVE_NUM);
+    }
+    for (Product prod : product) {
+      if (prod.getProductID() == prodID) {
+        return prod;
+      }
+    }
+    throw new IllegalArgumentException(MSG_PRODUCT_NOT_FOUND);
+  }
+
+  public Order getOrderByID(int orderID) {
+    if (orderID < 0) {
+      throw new IllegalArgumentException(MSG_NEGATIVE_NUM);
+    }
+
+    for (Order ord : order) {
+      if (ord.getOrderID() == orderID) {
+        return ord;
+      }
+    }
+    throw new IllegalArgumentException(MSG_ORDER_NOT_FOUND);
   }
 }
 /*
