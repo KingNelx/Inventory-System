e56f9d8f4898cdc5c81708a7018c4a6838640197
KingNelx <tapiajonel606@gmail.com>
2023-02-13 21:55:33 +0800

    constructing product class

---
+++ Product.java
@@ -0,0 +1,50 @@
+public class Product {
+    private int productID;
+    private String productName;
+    private String shipFrom;
+    private double price;
+
+
+    private static final String emptyString = " ";
+    private static final String MSG_STRING_EMPTY = " Empty String Detected ";
+    private static final String MSG_NEGATIVE_DETECTED = " Negative Number Detected ";
+    private static final String MSG_STRING_NULL = " Null Detected ";
+
+    public Product (int productID, String productName, String shipFrom, double price) {
+
+    }
+
+    public void setProductID () {
+
+    }
+
+    public int getProductID () {
+
+    }
+
+
+    public void setProductName () {
+
+    }
+
+    public String getProductName () {
+
+    }
+
+    public void setShipFrom () {
+
+    }
+
+    public String getShipFrom () {
+
+    }
+
+    public void setProductPrice () {
+
+    }
+
+    public double getProductPrice () {
+
+    }
+    
+}
