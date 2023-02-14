55ead77c3c52d9cbc368ba797693950c49d65785
KingNelx <tapiajonel606@gmail.com>
2023-02-14 11:37:50 +0800

    adde list and two methods for adding products and orders

--- Index.java
+++ Index.java
@@ -2,7 +2,8 @@ public class Index {
     public static void main(String[] args) {
         Product myProduct = new Product(1, "Keyboard", "Canada", 500);
         Product myProduct2 = new Product(2, "Mouse", "Mexico", 700);
-        
-        System.out.println(myProduct.isSameProduct(myProduct2));
+        Order s1 = new Order(1, "Jonel Tapia", myProduct2);
+
+       
     }
 }
--- Inventory.java
+++ Inventory.java
@@ -1,3 +1,28 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.LogManager;
+
 public class Inventory {
-    
+
+  private static final String ERROR_PROD_NOT_FOUND = " Product not FOUND ";
+  private static final String ERROR_ORDER_NOT_FOUND = " Order not FOUND ";
+
+  public Inventory() {}
+
+  private List<Product> myProd = new ArrayList<>();
+  private List<Order> myOrder = new ArrayList<>();
+
+  public void addProduct(Product prod) {
+    if (prod == null) {
+        throw new IllegalArgumentException(ERROR_PROD_NOT_FOUND);
+    }
+    myProd.add(prod);
+  }
+
+  public void addOrder(Order ord){
+    if (ord == null) {
+        throw new IllegalArgumentException(ERROR_ORDER_NOT_FOUND);
+    }
+    myOrder.add(ord);
+  }
 }
