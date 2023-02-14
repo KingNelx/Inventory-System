cb5eb8506039b2ddaa395ea438d2666eb8fec6af
KingNelx <tapiajonel606@gmail.com>
2023-02-14 19:01:25 +0800

    new ideas = delete all codes (old)

--- Index.java
+++
@@ -1,9 +0,0 @@
-public class Index {
-    public static void main(String[] args) {
-        Product myProduct = new Product(1, "Keyboard", "Canada", 500);
-        Product myProduct2 = new Product(2, "Mouse", "Mexico", 700);
-        Order s1 = new Order(1, "Jonel Tapia", myProduct2);
-
-       
-    }
-}
